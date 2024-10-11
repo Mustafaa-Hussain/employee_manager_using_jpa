39cce7b6f324d09b5d4497ea62e294639900ef12
Mustafa Hussain <51909599+chemical-son@users.noreply.github.com>
2024-10-11 16:11:46 +0300

    init employee manager
    we will implement this app by using spring data JPA

---
+++ src/main/java/com/example/employee_manager_using_jpa/EmployeeManagerUsingJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_manager_using_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagerUsingJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagerUsingJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employee_manager_using_jpa/EmployeeManagerUsingJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee_manager_using_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagerUsingJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
